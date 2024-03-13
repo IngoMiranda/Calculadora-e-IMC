450e4ad9d7ef716ccb6feef0ff747e15ef8db7b7
IngoMiranda <ingolima26@gmail.com>
2024-03-13 06:33:36 -0300

    add mudança de tela e especificações

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ const weightInput = document.querySelector("#weight");
 const calcBtn = document.querySelector("#calc-btn");
 const cleaBtn = document.querySelector("#clear-btn");
 
+const imcNumber = document.querySelector("#imc-number span");
+const imcInfo = document.querySelector("#imc-infor span");
+const backBtn = document.querySelector("#back-btn");
+
+const calcContainer = document.querySelector("#calc-container");
+const resultContainer = document.querySelector("#result-container");
+
 //função
 function creatTable(data) {
     data.forEach(item => {
@@ -70,8 +77,11 @@ function creatTable(data) {
 };
 
 function clearInputs() {
+    //limpar inputs 
     heightInput.value = "";
     weightInput.value = "";
+    imcNumber.classList = "";
+    imcInfo.classList = "";
 };
 
 function validDigits(text) {
@@ -79,18 +89,88 @@ function validDigits(text) {
     // validação de texto: Permitindo apenas escrever números e com vírgulas
 };
 
+function calcImc(weight, height){
+    // calculo do imc
+    const imc = (weight /(height * height)).toFixed(1);
+    return imc;
+};
+
+function showHide(){
+    // exibir e ocultar próxima tela
+    calcContainer.classList.toggle("hide");
+    resultContainer.classList.toggle("hide");
+};
+
 creatTable(data);
 
 
 [heightInput, weightInput].forEach((el) => {
+    // validando os valores dos inputs baseado no tratamento com expressão regular
     el.addEventListener("input", (e) => {
         const updateValue = validDigits(e.target.value);
         e.target.value = updateValue;
     });
 });
 
+calcBtn.addEventListener("click", (e)=>{
+    // excução da ação de calculo e mudança de tala
+    e.preventDefault();
+    const weight = +weightInput.value.replace(",", "."); // tratamento de "," para "."
+    const height = +heightInput.value.replace(",", ".");
+
+    if(!weight || !height) return;
+
+    const imc = calcImc(weight, height); // executando calculo
+
+    let info;
+    
+    data.forEach((item)=>{
+        // buscando informações no data
+        if(imc >= item.min && imc <= item.max){
+            info = item.infor;
+        };
+    });
+    
+    if(!info) return; // verificação de valor não valido
+    
+    imcNumber.innerText = imc; // saída de informações precessadas
+    imcInfo.innerText = info;
+
+    switch(info){
+        case "Magreza":
+            imcNumber.classList.add("low");
+            imcInfo.classList.add("low");
+            break;
+        case "Normal":
+            imcNumber.classList.add("good");
+            imcInfo.classList.add("good");
+            break;
+        case "Sobrepeso":
+            imcNumber.classList.add("low");
+            imcInfo.classList.add("low");
+            break;
+        case "Obesidade":
+            imcNumber.classList.add("medium");
+            imcInfo.classList.add("medium");
+            break;
+        case "Obesidade grave":
+            imcNumber.classList.add("high");
+            imcInfo.classList.add("high");
+            break;    
+
+    }
+
+    showHide();
+
+});
 
 cleaBtn.addEventListener("click", (e) => {
     e.preventDefault();
     clearInputs();
+});
+
+backBtn.addEventListener("click", () =>{
+    // a função está sem o parametro de "e" pois o botão está fora do form
+    clearInputs();
+    showHide();
 });
